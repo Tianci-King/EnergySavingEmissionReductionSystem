acab6f29a3d2cc4c77c31310dea504d509475415
JAYJAY <2422301985@qq.com>
2024-02-25 02:18:29 +0800

    feat(latitudeCard):新增维度卡片

--- src/components/Home/latitudeCard.vue
+++ src/components/Home/latitudeCard.vue
@@ -3,18 +3,30 @@
   display: flex;
   flex-direction: column;
 }
+.checkBox{
+  padding: 5px;
+}
+.choose{
+  color:green;
+}
 </style>
 
 <template>
 <div class="card">
-  <a-image :preview="false" @click="handleImgClick" :src="latitude.img" width="190"></a-image>
-  <a-checkbox v-model="isChecked" :value="latitude.name">{{latitude.name}}</a-checkbox>
+  <a-checkbox @click="handleChange" :value="latitude.name">
+    <template #checkbox="">
+      <a-image :preview="false" :src="latitude.img" width="165"></a-image>
+    </template>
+  </a-checkbox>
+  <a-checkbox @click="handleChange" :value="latitude.name" class="checkBox" >
+    <span :class="{'choose':isChoose}">{{latitude.name}}</span>
+  </a-checkbox>
 </div>
 </template>
 
 <script setup lang="ts">
 
-import {onMounted, ref} from "vue";
+import {ref} from "vue";
 
 const props = defineProps({
   latitude: {
@@ -23,10 +35,11 @@ const props = defineProps({
   }
 })
 
-const isChecked = ref(false);
+const isChoose = ref(false)
 
-const handleImgClick = () => {
-  console.log(isChecked.value);
-  isChecked.value = !isChecked.value;
+const handleChange = () => {
+  isChoose.value = !isChoose.value
+  console.log(isChoose.value)
 }
+
 </script>
